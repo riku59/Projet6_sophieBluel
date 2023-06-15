04b10750b9e315db826ed924248d725528650f35
riku59 <123378420+riku59@users.noreply.github.com>
2023-06-15 15:19:52 +0200

    Ajout des travaux recupérer sur le back-end a la galerie et suppression des ancien projets

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -1,17 +1,41 @@
+let categories = [];
+let work = [];
+const galery = document.querySelector(".gallery");
+
 //-----------------
 //récupération des API
 //*****************
 
-// const fetchCategory = () => {
-fetch("http://localhost:5678/api/categories")
-  .then((response) => response.json())
-  .then((dataCategories) => console.log(dataCategories));
+const fetchCategory = async () => {
+  await fetch("http://localhost:5678/api/categories")
+    .then((response) => response.json())
+    .then((dataCategories) => (categories = dataCategories));
 
-// };
+  console.log(categories);
+};
 
-// const fetchWork = () => {
-fetch("http://localhost:5678/api/works")
-  .then((response) => response.json())
-  .then((dataWork) => console.log(dataWork));
+const fetchWork = async () => {
+  await fetch("http://localhost:5678/api/works")
+    .then((response) => response.json())
+    .then((dataWork) => (work = dataWork));
 
-// };
+  console.log(work);
+};
+
+const display = async () => {
+  await fetchCategory();
+  await fetchWork();
+  galery.innerHTML = work
+    .map(
+      (projet) =>
+        `
+      <figure>
+      <img src=${projet.imageUrl} alt="photo de ${projet.title}">
+      <figcaption>${projet.title}</figcaption>
+      </figure>
+  `
+    )
+    .join("");
+};
+
+display();
