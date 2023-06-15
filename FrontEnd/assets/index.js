515a5c66fec08c19f10073f2c4814cc625fbb0f0
riku59 <123378420+riku59@users.noreply.github.com>
2023-06-15 19:16:33 +0200

    ajout des bouton, a remettre dans l'ordre

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -1,6 +1,7 @@
+const galery = document.querySelector(".gallery");
+const portFolio = document.getElementById("portfolio");
 let categories = [];
 let work = [];
-const galery = document.querySelector(".gallery");
 
 //-----------------
 //récupération des API
@@ -8,15 +9,30 @@ const galery = document.querySelector(".gallery");
 
 const fetchCategory = async () => {
   await fetch("http://localhost:5678/api/categories")
-    .then((response) => response.json())
+    .then((response) => response.json()) //Méthode .json() => méthode qui renvoie le body de la requête.
     .then((dataCategories) => (categories = dataCategories));
 
   console.log(categories);
+  const buttonDiv = document.createElement("div");
+  const addButton = document.createElement("button");
+  addButton.classList.add("button_form");
+  buttonDiv.classList.add("button_div");
+  addButton.textContent = " Tous";
+
+  portFolio.appendChild(buttonDiv);
+  categories.forEach((categorie) => {
+    const addButton = document.createElement("button");
+    buttonDiv.appendChild(addButton);
+    addButton.classList.add("button_form");
+    addButton.textContent = categorie.name;
+    buttonDiv.classList.add("button_div");
+  });
+  buttonDiv.appendChild(addButton);
 };
 
 const fetchWork = async () => {
   await fetch("http://localhost:5678/api/works")
-    .then((response) => response.json())
+    .then((response) => response.json()) //Méthode .json() => méthode qui renvoie le body de la requête.
     .then((dataWork) => (work = dataWork));
 
   console.log(work);
@@ -30,12 +46,11 @@ const display = async () => {
       (projet) =>
         `
       <figure>
-      <img src=${projet.imageUrl} alt="photo de ${projet.title}">
-      <figcaption>${projet.title}</figcaption>
+        <img src=${projet.imageUrl} alt="photo de ${projet.title}">
+        <figcaption>${projet.title}</figcaption>
       </figure>
   `
     )
     .join("");
 };
-
 display();
