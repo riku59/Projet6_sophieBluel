4d1414e2f9a89e7705420ddd08cbb1d2cc20e110
riku59 <123378420+riku59@users.noreply.github.com>
2023-06-15 09:57:16 +0200

    récupération des donné des API works et categories

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -1,1 +1,17 @@
+//-----------------
+//récupération des API
+//*****************
 
+// const fetchCategory = () => {
+fetch("http://localhost:5678/api/categories")
+  .then((response) => response.json())
+  .then((dataCategories) => console.log(dataCategories));
+
+// };
+
+// const fetchWork = () => {
+fetch("http://localhost:5678/api/works")
+  .then((response) => response.json())
+  .then((dataWork) => console.log(dataWork));
+
+// };
