f3429c4707fc776a08a2ef3d284779a932b34334
riku59 <123378420+riku59@users.noreply.github.com>
2023-06-20 11:02:12 +0200

    explication de la partie test de l'identifiant + mot de passe

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -13,9 +13,6 @@ function requestLogin() {
   });
 }
 
-//-------------------------------
-// Vérification de l'e-mail
-//-------------------------------
 const mailInput = document.getElementById("email");
 // console.log(mailInput);
 const passwordInput = document.getElementById("password");
@@ -25,6 +22,9 @@ const errorDisplay = document.getElementById("notFound");
 // console.log(submitLog);
 // console.log(errorDisplay);
 
+//-------------------------------
+// Vérification de l'e-mail
+//-------------------------------
 const emailCheker = (value) => {
   const errorMail = document.querySelector(".log_form span");
 
@@ -43,11 +43,13 @@ mailInput.addEventListener("input", (e) => {
   console.log(e.target.value);
   emailCheker(e.target.value); // fonction pour vérifier la forme de l'émail.
 });
+
+//----------------------------------------------------
+
 passwordInput.addEventListener("input", (e) => {
   console.log(e.target.value);
 });
 
-//----------------------------------------------------
 // test de l'identifiant + mot de passe
 submitLog.addEventListener("click", (e) => {
   e.preventDefault();
@@ -56,11 +58,10 @@ submitLog.addEventListener("click", (e) => {
   requestLogin()
     .then((Response) => Response.json())
     .then((login) => {
-      console.log(login); // envoie le message "user not found"
+      console.log(login); //envoie le UserID et le token lorsque l'identifiant est bon.
       if (login.token) {
-        localStorage.setItem("token", login.token);
-        isUserLogged = true;
-        window.location.href = "./index.html";
+        localStorage.setItem("token", login.token); //stock le token dans le localStorage
+        // window.location.href = "./index.html"; // renvoie a la page index.html
         console.log("Utilisateur connécté");
       } else {
         console.error(" token introuvable");
