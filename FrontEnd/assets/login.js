efa5b29cf7f4816192cbc442d96a9024c7a961c9
riku59 <123378420+riku59@users.noreply.github.com>
2023-06-20 10:36:50 +0200

    création de la page LogIn, ajout des fonctionnalité de la navbar. Connexion a faire

---
+++ FrontEnd/assets/login.js
@@ -0,0 +1,70 @@
+function requestLogin() {
+  return fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      // transforme en json
+      email: stockEmail,
+      password: stockPassword,
+    }),
+    credentials: "same-origin",
+  });
+}
+
+//-------------------------------
+// Vérification de l'e-mail
+//-------------------------------
+const mailInput = document.getElementById("email");
+// console.log(mailInput);
+const passwordInput = document.getElementById("password");
+// console.log(passwordInput);
+const submitLog = document.querySelector('input[type="submit"');
+const errorDisplay = document.getElementById("notFound");
+// console.log(submitLog);
+console.log(errorDisplay);
+
+const emailCheker = (value) => {
+  const errorMail = document.querySelector(".log_form span");
+
+  if (
+    !value.match(/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/)
+  ) {
+    errorMail.classList.add("errorEmail");
+    errorMail.textContent = "Email incorrect";
+  } else {
+    errorMail.classList.remove("errorEmail");
+    errorMail.textContent = "";
+  }
+};
+
+mailInput.addEventListener("input", (e) => {
+  console.log(e.target.value);
+  emailCheker(e.target.value); // fonction pour vérifier la forme de l'émail.
+});
+passwordInput.addEventListener("input", (e) => {
+  console.log(e.target.value);
+});
+stockEmail = mailInput.value;
+stockPassword = passwordInput.value;
+//----------------------------------------------------
+// test de l'identifiant + mot de passe
+submitLog.addEventListener("click", (e) => {
+  e.preventDefault();
+  requestLogin()
+    .then((Response) => Response.json())
+    .then((login) => {
+      console.log(login); // envoie le message "user not found"
+      if (login.token) {
+        localStorage.setItem("token", login.token);
+        isUserLogged = true;
+        window.location.href = "./index.html";
+        console.log("Utilisateur connécté");
+      } else {
+        console.error("Le token n'a pas été trouvé");
+        errorDisplay.textContent = "Identifiant ou Mot de passe incorrect";
+        errorDisplay.classList.add("notFound");
+      }
+    });
+});
